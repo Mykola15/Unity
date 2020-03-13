cfc122a311fb4fafd2b6af06ec6ca737a5fe6cd2
Koshlan <mykola.koshlan@gmail.com>
2020-03-13 09:28:52 +0200

    add achievement component

---
+++ src/components/User/Achievement/achievement.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './achievement.scss';
+
+function Achievement() {
+    return (
+        <div className="achievement-block">
+            <div className="achievement-img"><img src="achievement.png" alt=""/></div>
+            <div className="achievement-description"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+            </div>
+        </div>
+    );
+}
+
+export default Achievement;
