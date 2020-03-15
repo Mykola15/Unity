b47b674519c511ab6f70a174beda092488152f75
Khanas Yura <hanas2000@i.ua>
2020-03-15 22:35:49 +0200

    redesign page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -7,19 +7,19 @@ function Login() {
      <div className="container">
         <div className="signin-content row">
             <div className="signin-form col-md-6 col-xs-12">
-                <h2 className="form-title">Sign in</h2>
+                <h2 className="form-title">Login</h2>
                 <form method="POST" className="register-form" id="login-form">
                     <div className="form-group">
-                        <label><i className="zmdi zmdi-account material-icons-name"></i></label>
+                        <label className="name"><i className="zmdi zmdi-account material-icons-name"></i></label>
                         <input type="text" id="your_name" placeholder="Your Name"/>
                     </div>
                     <div className="form-group">
-                        <label><i className="zmdi zmdi-lock"></i></label>
+                        <label className="pass"><i className="zmdi zmdi-lock"></i></label>
                         <input type="password" id="your_pass" placeholder="Password"/>
                     </div>
-                    <div className="form-group">
+                    <div className="form-group  remmeber">
                         <input type="checkbox" id="remember-me" className="agree-term" />
-                        <label className="label-agree-term"><span><span></span></span>Remember me</label>
+                        <label className="label-agree-term">Remember me</label>
                     </div>
                     <div className="form-group form-button">
                         <input type="submit" id="signin" className="form-submit" value="Log in"/>
@@ -33,10 +33,10 @@ function Login() {
                         <li><a href="#"><i className="display-flex-center zmdi zmdi-google"></i></a></li>
                     </ul>
                 </div>
+                <span className="login-label">Not a member?</span><a href="#" className="signin-image-link">Sign Up</a>
             </div>
             <div className="signin-image col-md-6 col-xs-12">
                 <figure><img src={photo} alt="sing in image"/></figure>
-                <a href="#" className="signin-image-link">Create an account</a>
             </div>
         </div>
     </div>);
