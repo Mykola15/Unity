92a116750598fc7c957f34c7afe48e96c66eafcc
Nazar Koval <nazar_koval@epam.com>
2020-03-20 15:26:24 +0200

    Temporary commit

--- src/components/User/UserInfo/UserInfo.tsx
+++ src/components/User/UserInfo/UserInfo.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import avatar from './avatar.png';
 import './UserInfo.scss';
 
 function UserInfo() {
     return (
         <div className="user-info-block">
-
-        </div>
+		<div className="container">
+    			<div className="rightPart">
+            				<img src={avatar} className="avatar" alt="Avatar"/>
+            				<div className="form-group">
+                				<p className="person-name">Nazar Koval</p>
+            				</div>
+           			 <div className="form-group">
+                			<p className="person-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quisnostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecatcupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            			</div>
+    			</div>
+			</div>
+	</div>
     );
 }
 
