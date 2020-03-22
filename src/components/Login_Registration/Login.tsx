ed09015b7bf853b086c0f98c445591382b90abbc
Yura Khanas <hanasura79@gmail.com>
2020-03-22 19:54:30 +0200

    Add login and registration page

--- src/components/Login_Registration/Login.tsx
+++ src/components/Login_Registration/Login.tsx
@@ -4,6 +4,46 @@ import './LoginRegister.scss';
 
 function Login() {
     return (
+<<<<<<< HEAD
+    <div className="main-sign-wrapper">
+     <div className="container main">
+        <div className="signin-content row">
+            <div className="signin-form col-md-6 col-xs-12">
+                <h2 className="form-title">Login</h2>
+                <form method="POST" className="register-form" id="login-form">
+                    <div className="form-group">
+                        <label><i className="fas fa-user material-icons-name"></i></label>
+                        <input type="text" className="form" id="your_name" placeholder="Your Name"/>
+                    </div>
+                    <div className="form-group">
+                        <label><i className="fas fa-lock"></i></label>
+                        <input type="password" className="form" id="your_pass" placeholder="Password"/>
+                    </div>
+                    <div className="form-group  remmeber">
+                        <input type="checkbox" id="remember-me" className="agree-term" />
+                        <label className="label-agree-term">Remember me</label>
+                    </div>
+                    <div className="form-group form-button">
+                        <input type="submit" id="signin" className="form-submit" value="Log in"/>
+                    </div>
+                </form>
+                <div className="social-login">
+                    <span className="social-label">Or login with</span>
+                    <ul className="socials">
+                        <li><a href="https://uk-ua.facebook.com"><i className="fab fa-facebook-f"></i></a></li>
+                        <li><a href="https://twitter.com/?lang=uk"><i className="fab fa-twitter"></i></a></li>
+                        <li><a href="https://www.google.com.ua/?hl=ru"><i className="fab fa-google"></i></a></li>
+                    </ul>
+                </div>
+                <span className="login-label">Not a member?</span><a href="/registration" className="signin-image-link">Sign Up</a>
+            </div>
+            <div className="signin-image col-md-6 col-xs-12">
+                <figure><img src={photo} className="image" alt="sing in"/></figure>
+            </div>
+        </div>
+    </div>
+    </div>);
+=======
         <div className="main-sign-wrapper">
             <div className="container">
                 <div className="signin-content row">
@@ -46,6 +86,7 @@ function Login() {
                 </div>
             </div>
         </div>);
+>>>>>>> 2bd171ad5636ed2205beb9d92e881289302ea7be
 }
 
 export default Login;
--- src/components/Login_Registration/Registration.tsx
+++ src/components/Login_Registration/Registration.tsx
@@ -4,6 +4,50 @@ import './LoginRegister.scss';
 
 function Login() {
     return (
+<<<<<<< HEAD
+    <div className="main-sign-wrapper">
+     <div className="container main">
+        <div className="signin-content row">
+            <div className="signin-form col-md-6 col-xs-12">
+                <h2 className="form-title">Sign Up</h2>
+                <form method="POST" className="register-form" id="login-form">
+                    <div className="form-group">
+                        <label><i className="fas fa-user material-icons-name"></i></label>
+                        <input type="text" className="form" id="your_name" placeholder="Your Name"/>
+                    </div>
+                    <div className="form-group">
+                        <label className="email"><i className="fas fa-envelope"></i></label>
+                        <input type="email" className="form" placeholder="Email"/>
+                    </div>
+                    <div className="form-group">
+                        <label><i className="fas fa-lock"></i></label>
+                        <input type="password" className="form" placeholder="Password"/>
+                    </div>
+                    <div className="form-group">
+                        <label><i className="fas fa-lock"></i></label>
+                        <input type="password" className="form" placeholder="Confirm password"/>
+                    </div>
+                    <div className="form-group form-button">
+                        <input type="submit" id="signin" className="form-submit" value="Register"/>
+                    </div>
+                </form>
+                <div className="social-login">
+                    <span className="social-label">Or register with</span>
+                    <ul className="socials">
+                        <li><a href="https://uk-ua.facebook.com"><i className="fab fa-facebook-f"></i></a></li>
+                        <li><a href="https://twitter.com/?lang=uk"><i className="fab fa-twitter"></i></a></li>
+                        <li><a href="https://www.google.com.ua/?hl=ru"><i className="fab fa-google"></i></a></li>
+                    </ul>
+                </div>
+                <span className="login-label">Have account?</span><a href="/login" className="signin-image-link">Sign In</a>
+            </div>
+            <div className="signin-image col-md-6 col-xs-12">
+                <figure><img src={photo} className="image" alt="sing up"/></figure>
+            </div>
+        </div>
+    </div>
+</div>);
+=======
         <div className="main-sign-wrapper">
             <div className="container">
                 <div className="signin-content row">
@@ -50,6 +94,7 @@ function Login() {
                 </div>
             </div>
         </div>);
+>>>>>>> 2bd171ad5636ed2205beb9d92e881289302ea7be
 }
 
 export default Login;
